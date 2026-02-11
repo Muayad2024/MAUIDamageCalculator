c79695c87b03d3c866fe81435d056bbe60b12806
Muayad2024 <muayadtrade@gmail.com>
2026-02-11 20:30:47 +0800

    Add project files.

---
+++ MAUIDamageCalculator/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection.PortableExecutable;
+
+namespace MAUIDamageCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        SwordDamage swordDamage = new SwordDamage();
+        public MainPage()
+        {
+            InitializeComponent();
+            //swordDamage.SetFlaming(Flaming.IsChecked);
+            //swordDamage.SetMagic(Magic.IsChecked); 
+            RollDice();
+
+        }
+
+        private void RollDice()
+        {
+            swordDamage.Roll = Random.Shared.Next(1, 7) + Random.Shared.Next(1, 7)
+                + Random.Shared.Next(1, 7);
+            DisplayDamage();
+        }
+
+        private void DisplayDamage()
+        {
+            //Damage.Text = $"Rolled {swordDamage.Roll} for {swordDamage.Damage} HP";
+
+        }
+
+        private void Flaming_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            swordDamage.SetFlaming(e.Value); 
+            DisplayDamage();
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        { 
+        
+        
+        }
+    }
+}
