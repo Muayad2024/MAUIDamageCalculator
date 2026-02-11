687fd44e9ff89a6b1bd95045da4cc11841366c43
Muayad2024 <muayadtrade@gmail.com>
2026-02-11 20:30:47 +0800

    Add project files.

---
+++ MAUIDamageCalculator/SwordDamage.cs
@@ -0,0 +1,49 @@
+﻿namespace MAUIDamageCalculator
+{
+    
+    
+        internal class SwordDamage
+        {
+            public const int BASE_DAMAGE = 3;
+            public const int FLAME_DAMAGE = 2;
+
+            //fields of the class
+            public int Roll;
+            public decimal MagicMultiplier = 1M;
+            public int Damage;  //why we didn't initialize it here?
+
+            public void CalculateDamage() //==============================
+            {
+                Damage = (int)(Roll * MagicMultiplier) + BASE_DAMAGE;
+            }
+            public void SetMagic(bool isMagic) //=============================
+            {
+                if (isMagic)
+                {
+                    MagicMultiplier = 1.75M;
+                }
+                else
+                {
+                    MagicMultiplier = 1M;
+                }
+                CalculateDamage(); //assign the new MagicMultiplier value to it 
+
+            }
+            public void SetFlaming(bool isFlaming) //=========================
+            {
+                CalculateDamage();
+                if (isFlaming)
+                {
+                    Damage += FLAME_DAMAGE;
+                }
+
+
+            }
+
+
+
+
+        
+    }
+
+}
